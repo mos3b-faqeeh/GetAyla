473af99e0912570dadeb616e88df34b7c47cc1ce
mosab <mos3b.faqeeh@gmail.com>
2021-04-05 18:13:43 -0700

    add host

--- GetAylaDev/GetAylaDev/settings.py
+++ GetAylaDev/GetAylaDev/settings.py
@@ -75,13 +75,17 @@ WSGI_APPLICATION = 'GetAylaDev.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
