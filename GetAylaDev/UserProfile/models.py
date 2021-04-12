84971d9a86b646be30e6bb1d47d8accceee6bd0e
mosab <mos3b.faqeeh@gmail.com>
2021-04-11 20:11:07 -0700

    make save for settings as another draft

--- GetAylaDev/UserProfile/models.py
+++ GetAylaDev/UserProfile/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Profiles(models.Model):
+    profileInsta = models.CharField(max_length=200)
+    InstaDesk = models.CharField(max_length=200)
