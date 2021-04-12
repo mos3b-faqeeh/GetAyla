b122c615ca78d466c09e1020e31c7aba5bcb6c27
mosab <mos3b.faqeeh@gmail.com>
2021-04-11 21:09:46 -0700

    Configure DB

--- GetAylaDev/GetAylaDev/settings.py
+++ GetAylaDev/GetAylaDev/settings.py
@@ -128,5 +128,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
+STATIC_ROOT=os.path.join(BASE_DIR,'assets')
 
-STATIC_ROOT = "/var/www/getayla.io.com/static/"
+
