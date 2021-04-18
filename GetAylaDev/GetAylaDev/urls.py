70976ac5e86a57d260707462c1d35a1ffc296b93
mosab <mos3b.faqeeh@gmail.com>
2021-04-17 23:33:25 -0700

    Adding register

--- GetAylaDev/GetAylaDev/urls.py
+++ GetAylaDev/GetAylaDev/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('', include('CurrentDashboard.urls')),
     path('', include('home.urls')),
     path('services/', include('home.urls')),
-
     path('register/', include('Accounts.urls')),
 
 
--- GetAylaDev/home/urls.py
+++ GetAylaDev/home/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
 path('', views.home,name="home"),
-path('services', views.services,name="services"),
+path('', views.services,name="services"),
 
 ]
