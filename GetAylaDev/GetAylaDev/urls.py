b4e5fb61f36c2f377e04c219f8348adb1504c271
mosab <mos3b.faqeeh@gmail.com>
2021-04-17 23:25:36 -0700

    Adding register

--- GetAylaDev/GetAylaDev/urls.py
+++ GetAylaDev/GetAylaDev/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('', include('UserProfile.urls')),
     path('', include('CurrentDashboard.urls')),
     path('', include('home.urls')),
+    path('services/', include('home.urls')),
+
     path('register/', include('Accounts.urls')),
 
 
--- GetAylaDev/home/views.py
+++ GetAylaDev/home/views.py
@@ -3,5 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 
+
+def services (request):
+    return render(request, 'services.html', {})
+
+
+
 def home (request):
     return render(request, 'home.html', {})
