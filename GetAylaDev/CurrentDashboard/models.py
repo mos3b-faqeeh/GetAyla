596da4e2d26c7e30dbe667e9a156e3c30e9c8351
mosab <mos3b.faqeeh@gmail.com>
2021-07-11 08:20:15 -0700

    Add tables into db Fixing homepage

--- GetAylaDev/CurrentDashboard/models.py
+++ GetAylaDev/CurrentDashboard/models.py
@@ -1,3 +1,72 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+
+
+class InstaProfiles(models.Model):
+    InstagramUserNameCustomer= models.CharField(max_length=200)
+    BusinessName = models.CharField(max_length=200)
+    InstagramPassword= models.CharField(max_length=200)
+    City = models.CharField(max_length=200)
+    country= models.CharField(max_length=200)
+    PostalCode= models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+
+
+
+
+
+class followers(models.Model):
+    followerUsername = models.CharField(max_length=200)
+    instagramLink= models.CharField(max_length=200)
+    relevancyScore= models.CharField(max_length=200)
+    location= models.CharField(max_length=200)
+    lastActivity= models.CharField(max_length=200)
+
+    created = models.DateTimeField(auto_now_add=True)
+    InstagramUserNameCustomer = models.ForeignKey(InstaProfiles,on_delete=models.SET_DEFAULT,default=1)
+
+
+class targets(models.Model):
+    targetUsername = models.CharField(max_length=200)
+    instagramLink= models.CharField(max_length=200)
+    relevancyScore= models.CharField(max_length=200)
+    location= models.CharField(max_length=200)
+    lastActivity= models.CharField(max_length=200)
+
+    created = models.DateTimeField(auto_now_add=True)
+    InstagramUserNameCustomer = models.ForeignKey(InstaProfiles,on_delete=models.SET_DEFAULT,default=1)
+
+
+
+
+
+class CustomerInteractionsOnFollowers(models.Model):
+    InstagramFollower = models.ForeignKey(followers,on_delete=models.SET_DEFAULT,default=1)
+    interactionType=models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    InstagramUserNameCustomer = models.ForeignKey(InstaProfiles,on_delete=models.SET_DEFAULT,default=1)
+
+
+class CustomerInteractionsOnTarget(models.Model):
+    InstagramTarget = models.ForeignKey(targets,on_delete=models.SET_DEFAULT,default=1)
+    interactionType=models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    InstagramUserNameCustomer = models.ForeignKey(InstaProfiles,on_delete=models.SET_DEFAULT,default=1)
+
+
+
+
+
+
+
+
+
+
+
+
--- GetAylaDev/UserProfile/models.py
+++ GetAylaDev/UserProfile/models.py
@@ -14,3 +14,4 @@ class InstaProfiles(models.Model):
     PostalCode= models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
 
+
